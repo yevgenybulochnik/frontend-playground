6e5f16fd343e9bbbe2ff960a8e9c01bc5c48ee0d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-20 19:19:56 -0700

    button.component.ts, typescript test

---
+++ webpack-demo/app/button.component.ts
@@ -0,0 +1,8 @@
+export class Button {
+  button_name: string;
+  type: string;
+    constructor(name: string){
+      this.button_name = name;
+      this.type = 'testbutton';
+    }
+}
--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -1,4 +1,5 @@
 import { greeter } from './greeter';
+import { Button } from '.button.component';
 
 var element = document.createElement('div');
 element.innerHTML = 'Welcome to webpack demo!';
@@ -7,3 +8,6 @@ document.body.appendChild(element);
 greeter("Hello this is a greeting", "red");
 greeter("This is another greeting", "lightblue");
 $('body').append('<div> jquery test </div>');
+
+var button_test = new Button('helloworld');
+console.log(button_test);
