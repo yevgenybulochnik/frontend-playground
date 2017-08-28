e9e454d6ccaae1233a5cc222b4995926625a0f07
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-08-27 20:29:33 -0700

    Chart sass, clean project components

--- electron-calendar/src/app/chart/chart.component.ts
+++ electron-calendar/src/app/chart/chart.component.ts
@@ -4,25 +4,11 @@ import * as d3 from 'd3';
 @Component({
   selector: 'd3-chart',
   template: `
-    <div>
-      <svg class="chart"></svg>
-    </div>
   `,
-  styles: [`
-
-    `]
+  styleUrls: ['./chart.component.sass']
 })
 export class ChartComponent {
   constructor(private elRef: ElementRef) {}
   ngAfterViewInit() {
-    let chart = d3.select(this.elRef.nativeElement).select('.chart')
-          .append('svg')
-          .attr('width', 50)
-          .attr('height', 50)
-          .append('circle')
-          .attr('cx', 25)
-          .attr('cy', 25)
-          .attr('r', 25)
-          .style('fill', 'purple')
   }
 }
