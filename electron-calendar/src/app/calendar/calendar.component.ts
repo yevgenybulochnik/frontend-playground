6df3b47d08312155dcfe071a0b754c449cd0c186
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-09-06 22:28:02 -0700

    Updated selector

--- electron-calendar/src/app/calendar/calendar.component.ts
+++ electron-calendar/src/app/calendar/calendar.component.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'd3-chart',
+  selector: 'd3-calendar',
   template: `
   `,
   styleUrls: ['./calendar.component.sass']
