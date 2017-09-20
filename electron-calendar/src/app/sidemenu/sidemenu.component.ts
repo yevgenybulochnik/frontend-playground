dd7192d4d78bd74b3e72d4b9c77a03a8594fcbc9
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-09-19 19:13:09 -0700

    SideMenu initial build out

--- electron-calendar/src/app/sidemenu/sidemenu.component.ts
+++ electron-calendar/src/app/sidemenu/sidemenu.component.ts
@@ -4,13 +4,48 @@ import { DataService } from '../services/data.service'
 @Component({
   selector: 'side-menu',
   template: `
-    <div>hello</div>
+  <div class='clinic-container'>
+    <span>Clinics</span>
+    <button *ngFor='let clinic of clinics' (click)='getProviders(clinic)' [class.selected]='clinic === selectedClinic'>{{clinic}}</button>
+  </div>
+  <div class='provider-container'>
+    <span>Providers</span>
+    <button *ngFor='let provider of providers'>{{provider}}</button>
+  </div>
   `,
   styles: [`
-
+    :host {
+      font-family: arial;
+    }
+    button {
+      margin: 0.1em 0.5em;
+      border-radius: 5px;
+      border: solid 1px #008CBA;
+      background-color: white;
+      outline: none;
+    }
+    button:hover {
+      background-color: #008CBA;
+      color: white;
+    }
+    span {
+      border-bottom: solid 1px grey;
+      margin-bottom: 2px;
+    }
+    .clinic-container, .provider-container {
+      display: flex;
+      flex-direction: column;
+      padding: 5px;
+    }
+    .selected {
+      background: #008CBA;
+      color: white;
+    }
     `],
 })
 export class SideMenuComponent {
+  clinics: string[];
+  providers: string[];
   constructor(private dataService: DataService) {
     console.log(this.dataService)
   }
