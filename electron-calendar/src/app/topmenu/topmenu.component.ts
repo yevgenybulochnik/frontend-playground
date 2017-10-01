329297c4a47bee45e6c41f1ac1f25ec4036d4458
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-10-01 13:08:34 -0700

    TopMenu ts file

---
+++ electron-calendar/src/app/topmenu/topmenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-menu',
+  template: `
+  <div>test</div>
+  `,
+  styles: [``]
+})
+export class TopMenuComponent {
+
+}
