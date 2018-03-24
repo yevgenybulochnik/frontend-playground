aa9bd23ecc0b11d40445cb80f3594ecfd16499fc
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-03-23 19:04:04 -0700

    square click state lift to board

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -11,16 +11,27 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          squares: Array(9).fill(null),
+      }
+    }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={this.handleClick(i)}
+      />
+    );
   }
 
   render() {
