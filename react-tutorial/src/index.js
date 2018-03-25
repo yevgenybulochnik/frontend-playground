ae548dfa056353cb88fed8f00fa5b04f7d329db8
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-03-24 21:10:52 -0700

    Board click handler

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
@@ -19,17 +13,24 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          squares: Array(9).fill(null),
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
     }
+  }
+
+  handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares})
+  }
+
   renderSquare(i) {
     return (
       <Square
         value={this.state.squares[i]}
-        onClick={this.handleClick(i)}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
