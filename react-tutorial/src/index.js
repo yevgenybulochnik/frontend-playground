f0dffc4a481bb127be3b88c4f10330671e01d8ea
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-03-26 22:30:14 -0700

    Toggle turns

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -14,7 +14,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onclick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -25,13 +25,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares})
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
   }
 
   renderSquare(i) {
@@ -44,7 +48,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
