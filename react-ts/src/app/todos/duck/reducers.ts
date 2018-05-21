d1ab4612dc6894f1218e6decd5b4d5cd011de168
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-05-21 21:12:02 +0000

    Restart reducer file

--- react-ts/src/app/todos/duck/reducers.ts
+++ react-ts/src/app/todos/duck/reducers.ts
@@ -1,19 +1,14 @@
-import { combineReducers } from 'redux'
 import {
   Action,
-  ADD_TODO,
-  TOGGLE_TODO,
-  SET_VISIBILITY_FILTER,
   VisibilityFilters
 } from './actions'
 
-const { SHOW_ALL } = VisibilityFilters
 
-export function visibilityFilter(state = SHOW_ALL, action: Action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter
-    default:
-      return state
-  }
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL as string,
+  todos: [] as any
+}
+
+function todoApp(state = initialState, action: Action) {
+  return state
 }
