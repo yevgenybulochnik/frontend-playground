1565c8531e4891e6778c6fcacbc1803d95d84c24
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-05-20 20:52:18 -0700

    Initial reducer setup

---
+++ react-ts/src/app/todos/duck/reducers.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters
+} from './actions'
+
+const { SHOW_ALL } = VisibilityFilters
