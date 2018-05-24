c1ce1db573e5cd51db73b4a82e61a9fda8204401
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-05-24 03:52:48 +0000

    Initial backend server setup

--- react-ts/src/server/server.ts
+++ react-ts/src/server/server.ts
@@ -1,4 +1,15 @@
 import * as http from 'http'
 import * as DotEnv from 'dotenv'
+import App from './express'
 
 DotEnv.config()
+
+const PORT = process.env.BACKEND_PORT || 3000
+const server = http.createServer(App)
+
+server.listen(PORT)
+server.on('listening', onListen)
+
+function onListen() {
+  console.log(`Listening on Port: ${PORT}`)
+}
