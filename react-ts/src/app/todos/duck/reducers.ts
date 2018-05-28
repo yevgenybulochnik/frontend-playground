bc1d7a558eda7cdd7b95680e9089d158de30c8ca
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-05-27 21:45:56 -0700

    Todo reducer setup and tests

--- react-ts/src/app/todos/duck/reducers.ts
+++ react-ts/src/app/todos/duck/reducers.ts
@@ -1,14 +1,45 @@
 import {
   Action,
-  VisibilityFilters
+  VisibilityFilters,
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
 } from './actions'
 
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL as string,
-  todos: [] as any
+  todos: [] as any[]
 }
 
-function todoApp(state = initialState, action: Action) {
-  return state
+export function todoApp(state = initialState, action: Action) {
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo: any, index: number) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        })
+      })
+    default:
+      return state
+  }
 }
